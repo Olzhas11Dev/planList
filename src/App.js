d768aba0614f929eac5c20437b678a0b9a2340a5
Olzhas <11olzhas.k@gmail.com>
2021-06-22 00:09:11 +1200

    v3

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import { useState } from "react"
 function App() {
   const [inputText, setInputText] = useState("")
   const [array, setArray] = useState([])
+
   function addToArray() {
     setArray([...array, inputText])
     setInputText("") // To clean input
+  }
+
+  function cleanList(inputText) {
+    setArray(array.filter((e) => e !== inputText))
   }
   console.log(inputText)
   console.log(array)
@@ -16,8 +21,8 @@ function App() {
     <div className="App">
       <h2>Plan list</h2>
       <InputSection inputText={inputText} setInputText={setInputText} addToArray={addToArray} />
-      {array.map((e) => (
-        <ListItems item={e} />
+      {array.map((e, key) => (
+        <ListItems item={e} cleanList={cleanList} key={key} />
       ))}
 
       <div className="item">Items: {array.length}</div>
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,10 +1,13 @@
 import React from "react"
 
 export default function ListItems(props) {
+  console.log(props)
   return (
     <div className="listItem">
       <p>{props.item}</p>
-      <button className="btnRemove">x</button>
+      <button onClick={() => props.cleanList(props.item)} className="btnRemove">
+        x
+      </button>
     </div>
   )
 }
